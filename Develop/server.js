0e8e23fa917878259cb309e2aea94966b9d089f6
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-10-29 00:02:38 +1100

    Added listening PORT

--- Develop/server.js
+++ Develop/server.js
@@ -2,10 +2,18 @@ const express = require ('express');
 const path = require ('path');
 const fs = require ('fs');
 const util = require ('util');
+const api = require('./routes/index');
+
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+app.use('/api', api);
 
+app.use(express.static('public'));
+
+app.listen(PORT, () => 
+console.log(`App listening at http://localhost:${PORT} 🚀 `));
