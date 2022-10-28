cd9b41f978f225d94035765275f85a590a5e4865
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-10-28 20:45:55 +1100

    Added api-route module

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,7 +1,37 @@
-// Add dependencies
+const api = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils');
 
-const db = require('../db/db.json');
-const fs = require('fs');
-const uuid = require ('uuid');
+// GET Route for retrieving API
 
-// HTML routes file
+api.get('/', (req, res) => {
+    readFromFile ('./db/db.json'.then (data) => res.json (JSON.parse(data)));
+});
+
+// POST Route for API
+
+api.post('/', (req, res) => {
+    console.log (req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+    
+        readAndAppend(newNote, './db/db.json');
+        res.json (`Note added successfully 🚀`);
+    }
+    else {
+        res.error ('Error in adding the Note');
+    }
+    });
+
+    module.exports = api;
